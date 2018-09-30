650ba8e606bee4e9ca5213181651c97e5647d09c
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2018-09-30 22:18:14 +0200

    Initial commit

---
+++ RandomPassword/Controllers/AuthController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RandomPassword.Data;
+using RandomPassword.Models;
+
+namespace RandomPassword.Controllers
+{
+    public class AuthController : Controller
+    {
+        private ApplicationDbContext _db;
+        private SignInManager<IdentityUser> _signInManager;
+        private UserManager<IdentityUser> _userManager;
+
+        public AuthController(ApplicationDbContext db, SignInManager<IdentityUser> signin, UserManager<IdentityUser> usermanager)
+        {
+            _db = db;
+            _signInManager = signin;
+            _userManager = usermanager;
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterModel model)
+        {
+            var exists = _db.Users.SingleOrDefault(u => u.Email.Equals(model.Username));
+            if (exists != null)
+                return View();
+
+            var result = await _userManager.CreateAsync(new IdentityUser(model.Username), model.Password);
+
+            if (result.Succeeded)
+                return RedirectToAction("Index", "Home");
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(UserRegisterModel model)
+        {
+            var user = _db.Users.SingleOrDefault(u => u.UserName.Equals(model.Username));
+
+            if (user == null)
+                return View();
+
+            var result = await _signInManager.PasswordSignInAsync(user, model.Password, true, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public string GeneratePassword()
+        {
+            var options = _userManager.Options.Password;
+
+            int length = options.RequiredLength;
+
+            bool nonAlphanumeric = options.RequireNonAlphanumeric;
+            bool digit = options.RequireDigit;
+            bool lowercase = options.RequireLowercase;
+            bool uppercase = options.RequireUppercase;
+
+            StringBuilder password = new StringBuilder();
+            Random random = new Random();
+
+            while (password.Length < length)
+            {
+                char c = (char)random.Next(32, 126);
+
+                password.Append(c);
+
+                if (char.IsDigit(c))
+                    digit = false;
+                else if (char.IsLower(c))
+                    lowercase = false;
+                else if (char.IsUpper(c))
+                    uppercase = false;
+                else if (!char.IsLetterOrDigit(c))
+                    nonAlphanumeric = false;
+            }
+
+            if (nonAlphanumeric)
+                password.Append((char)random.Next(33, 48));
+            if (digit)
+                password.Append((char)random.Next(48, 58));
+            if (lowercase)
+                password.Append((char)random.Next(97, 123));
+            if (uppercase)
+                password.Append((char)random.Next(65, 91));
+
+            return password.ToString();
+        }
+    }
+}
